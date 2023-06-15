35f4873e257fd7367620030d1f27c06e0061402f
Mohamed Salman <mohamed.akj@gmail.com>
2023-06-15 10:30:17 +0530

    Implemented update_note PUT API and verified respective unit test cases

--- src/tests/test_notes.py
+++ src/tests/test_notes.py
@@ -47,7 +47,8 @@ def test_read_note_incorrect_id(test_app, monkeypatch):
     assert response.status_code == 404
     assert response.json()["detail"] == "Note not found"
 
-def test_read_all_notes(test_app,monkeypatch):
+
+def test_read_all_notes(test_app, monkeypatch):
     test_data = [
         {"title": "something", "description": "something else", "id": 1},
         {"title": "someone", "description": "someone else", "id": 2},
@@ -61,3 +62,42 @@ def test_read_all_notes(test_app,monkeypatch):
     response = test_app.get("/notes")
     assert response.status_code == 200
     assert response.json() == test_data
+
+
+def test_update_note(test_app, monkeypatch):
+    # Arrange
+    test_update_data = {"title": "someone", "description": "someone else", "id": 1}
+
+    # Act
+    async def mock_get(id):
+        return True
+
+    monkeypatch.setattr(crud, "get", mock_get)
+
+    async def mock_put(id, payload):
+        return 1
+
+    monkeypatch.setattr(crud, "put", mock_put)
+
+    # Assert
+    response = test_app.put("/notes/1", content=json.dumps(test_update_data))
+    assert response.status_code == 200
+    assert response.json() == test_update_data
+
+
+@pytest.mark.parametrize(
+    "id, payload, status_code",
+    [
+        [1, {}, 422],
+        [1, {"description": "bar"}, 422],
+        [999, {"title": "foo", "description": "bar"}, 404],
+    ]
+)
+def test_update_note_invalid(test_app, monkeypatch, id, payload, status_code):
+    async def mock_get(id):
+        return None
+
+    monkeypatch.setattr(crud, "get", mock_get)
+
+    response = test_app.put(f"/notes/{id}/", content=json.dumps(payload),)
+    assert response.status_code == status_code
